3cfb91222e3c6d2c3bab49ea3f26a16e260c2253
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-01 16:00:20 -0300

    Update VaccinationControlFactory

--- backend/src/main/java/com/apirestvaccinationcontrol/resources/VaccineRegistrationResource.java
+++ backend/src/main/java/com/apirestvaccinationcontrol/resources/VaccineRegistrationResource.java
@@ -57,5 +57,4 @@ public class VaccineRegistrationResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
 }
--- backend/src/test/java/com/apirestvaccinationcontrol/tests/factory/VaccinationControlFactory.java
+++ backend/src/test/java/com/apirestvaccinationcontrol/tests/factory/VaccinationControlFactory.java
@@ -3,15 +3,27 @@ package com.apirestvaccinationcontrol.tests.factory;
 import java.time.Instant;
 
 import com.apirestvaccinationcontrol.dto.VaccinationControlDTO;
+import com.apirestvaccinationcontrol.entities.Recipient;
 import com.apirestvaccinationcontrol.entities.VaccinationControl;
+import com.apirestvaccinationcontrol.entities.VaccineRegistration;
 
 public class VaccinationControlFactory {
 	
 	public static VaccinationControl createVaccinationControl() {
-		return new VaccinationControl(1L, "Brazil", 1, Instant.now());
+		VaccinationControl control = new VaccinationControl(1L, "Brazil", 1, Instant.now());
+		control.getRecipients().add(new Recipient(1L, null, null, null, null, null));
+		control.getVaccines().add(new VaccineRegistration(3L, null));
+		return control;
 	}
 	
 	public static VaccinationControlDTO createVaccinationControlDTO() {
-		return new VaccinationControlDTO(createVaccinationControl());
+		VaccinationControl control = createVaccinationControl();
+		return new VaccinationControlDTO(control, control.getRecipients(), control.getVaccines());
+	}
+	
+	public static VaccinationControlDTO createVaccinationControlDTO(Long id) {
+		VaccinationControlDTO dto = createVaccinationControlDTO();
+		dto.setId(id);
+		return dto;
 	}
 }
