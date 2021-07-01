e1676cfababd83d0cfd784240322f7c0370ebd18
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-01 12:17:09 -0300

    VaccineRegistrationServiceTests with Mockito

---
+++ backend/src/test/java/com/apirestvaccinationcontrol/tests/services/VaccineRegistrationServiceTests.java
@@ -0,0 +1,124 @@
+package com.apirestvaccinationcontrol.tests.services;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.apirestvaccinationcontrol.dto.VaccineRegistrationDTO;
+import com.apirestvaccinationcontrol.entities.VaccineRegistration;
+import com.apirestvaccinationcontrol.repositories.VaccineRegistrationRepository;
+import com.apirestvaccinationcontrol.services.VaccineRegistrationService;
+import com.apirestvaccinationcontrol.services.exceptions.DatabaseException;
+import com.apirestvaccinationcontrol.services.exceptions.ResourceNotFoundException;
+import com.apirestvaccinationcontrol.tests.factory.VaccineRegistrationFactory;
+
+@ExtendWith(SpringExtension.class)
+public class VaccineRegistrationServiceTests {
+
+	@InjectMocks
+	private VaccineRegistrationService service;
+	
+	@Mock
+	private VaccineRegistrationRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	private long dependentId;
+	private VaccineRegistration vaccine;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		dependentId = 2L;
+		vaccine = VaccineRegistrationFactory.createVaccineRegistration();
+		
+		doNothing().when(repository).deleteById(existingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		
+		when(repository.findById(existingId)).thenReturn(Optional.of(vaccine));
+		when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		
+		when(repository.getOne(existingId)).thenReturn(vaccine);
+		doThrow(EntityNotFoundException.class).when(repository).getOne(nonExistingId);
+				
+		when(repository.save(ArgumentMatchers.any())).thenReturn(vaccine);
+	} 
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+			verify(repository, times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+			verify(repository, times(1)).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+			verify(repository, times(1)).deleteById(dependentId);
+	}
+	
+	@Test
+	public void findByIdShouldReturnVaccineRegistrationDTOWhenIdExists() {
+		VaccineRegistrationDTO result = service.findById(existingId);
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
+	}
+	
+	@Test
+	public void findAllShouldReturnVaccineRegistrationDTOWhenIdExists() {
+		List<VaccineRegistrationDTO> result = service.findAll();
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void updateShouldReturnVaccineRegistrationDTOWhenIdExists() {
+		VaccineRegistrationDTO dto = new VaccineRegistrationDTO();
+		VaccineRegistrationDTO result = service.update(existingId, dto);
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		VaccineRegistrationDTO dto = new VaccineRegistrationDTO(); 
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingId, dto);
+		});
+	}
+}
