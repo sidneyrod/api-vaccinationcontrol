2f88d062dede11cafc96d8e45eb7dbf83ee3134d
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-01 12:32:50 -0300

    RecipientServiceTests with Mockito

---
+++ backend/src/test/java/com/apirestvaccinationcontrol/tests/services/RecipientServiceTests.java
@@ -0,0 +1,124 @@
+package com.apirestvaccinationcontrol.tests.services;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.apirestvaccinationcontrol.dto.RecipientDTO;
+import com.apirestvaccinationcontrol.entities.Recipient;
+import com.apirestvaccinationcontrol.repositories.RecipientRepository;
+import com.apirestvaccinationcontrol.services.RecipientService;
+import com.apirestvaccinationcontrol.services.exceptions.DatabaseException;
+import com.apirestvaccinationcontrol.services.exceptions.ResourceNotFoundException;
+import com.apirestvaccinationcontrol.tests.factory.RecipientFactory;
+
+@ExtendWith(SpringExtension.class)
+public class RecipientServiceTests {
+
+	@InjectMocks
+	private RecipientService service;
+	
+	@Mock
+	private RecipientRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	private long dependentId;
+	private Recipient recipient;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		dependentId = 2L;
+		recipient = RecipientFactory.createRecipient();
+		
+		doNothing().when(repository).deleteById(existingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		
+		when(repository.findById(existingId)).thenReturn(Optional.of(recipient));
+		when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		
+		when(repository.getOne(existingId)).thenReturn(recipient);
+		doThrow(EntityNotFoundException.class).when(repository).getOne(nonExistingId);
+				
+		when(repository.save(ArgumentMatchers.any())).thenReturn(recipient);
+	} 
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+			verify(repository, times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+			verify(repository, times(1)).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+			verify(repository, times(1)).deleteById(dependentId);
+	}
+	
+	@Test
+	public void findByIdShouldReturnRecipientDTOWhenIdExists() {
+		RecipientDTO result = service.findById(existingId);
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
+	}
+	
+	@Test
+	public void findAllShouldReturnRecipientDTOWhenIdExists() {
+		List<RecipientDTO> result = service.findAll();
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void updateShouldReturnRecipientDTOWhenIdExists() {
+		RecipientDTO dto = new RecipientDTO();
+		RecipientDTO result = service.update(existingId, dto);
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		RecipientDTO dto = new RecipientDTO(); 
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingId, dto);
+		});
+	}
+}
